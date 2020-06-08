615e5d1c6d789bebec1fe0d9c65ac9c849005602
hussnainjav3d <hussnain.jav3d@gmail.com>
2020-06-08 05:09:39 +0500

    Adding Lunch Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Dinner from './dinner';
+import Lunch from './lunch';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<hr />
 			<Dinner dishName="Pizza" sweetDish="Gulab Jaman" />
 			<hr />
+			<Lunch />
 		</div>
 	);
 }
---
+++ src/lunch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Lunch() {
+	return (
+		<div>
+			<h1>Todays menu</h1>
+			<ul>
+				<li>Checken Jalfarazi</li>
+				<li>Roast Charga</li>
+				<li>Dhegi Chargha</li>
+			</ul>
+		</div>
+	);
+}
+export default Lunch;
